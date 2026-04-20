320787c7ab32f351328ea6233de916935e56296e
Rupesh933 <rupeshrana20274@gmail.com>
2026-04-20 17:48:52 +0530

    fix in OrderDetails.jsx

--- frontend/src/pages/OrderDetails.jsx
+++ frontend/src/pages/OrderDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
+import PublicLayout from '../Component/PublicLayout';
 
 const OrderDetails = () => {
 
@@ -20,7 +21,7 @@ const OrderDetails = () => {
         fetch (`http://127.0.0.1:8000/api/orders/by_order_number/${order_number}/`)
         .then(res => res.json() )
         .then(data => {
-            setOrderAddress(data);
+            setOrderItem(data);
         const totalAmount = data.reduce(
           (sum, item) => sum + item.food.item_price * item.quantity,
           0,
@@ -29,7 +30,30 @@ const OrderDetails = () => {
         })
     },[order_number])
   return (
-    <div>OrderDetails</div>
+    < PublicLayout >
+    <div className='container py-5'>
+        <h3 className='mb-4 text-primary'>
+            <i className='fas fa-receipt me-2'></i> Order #{order_number} Details
+        </h3>
+        <div className='row'>
+            <div className='col-md-8'>
+                {orderItem.map((item, index) => (
+                    <div className='card mb-3 shadow-sm'>
+                        <div className='row'>
+                            <div className='col-md-4'>
+                                ffffff
+                            </div>
+                            <div className='col-md-8'>
+                                
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className='col-md-4'></div>
+        </div>
+    </div>
+    </PublicLayout>
   )
 }
 
