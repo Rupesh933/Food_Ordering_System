5711a1534fe6c8e6d823690d207856b0e4465f0e
Rupesh Rana <rupeshrana20274@gmail.com>
2026-04-21 17:23:59 +0530

    create Delivery status in OrderDetails.jsx page

--- frontend/src/pages/OrderDetails.jsx
+++ frontend/src/pages/OrderDetails.jsx
@@ -28,6 +28,12 @@ const OrderDetails = () => {
         ); // calcualte total price
         setTotal(totalAmount)
         })
+
+        fetch(`http://127.0.0.1:8000/api/order_address/${order_number}/`)
+        .then(res => res.json())
+        .then(data => {
+            setOrderAddress(data)
+        })
     },[order_number])
   return (
     < PublicLayout >
@@ -41,16 +47,43 @@ const OrderDetails = () => {
                     <div className='card mb-3 shadow-sm'>
                         <div className='row'>
                             <div className='col-md-4'>
-                                ffffff
+                                <img
+                        src={`http://127.0.0.1:8000${item.food.image}`}
+                        alt="food image"
+                        className="img-fluid rounded-start"
+                        style={{ maxHeight: "300px" }}
+                            />
                             </div>
                             <div className='col-md-8'>
-                                
+                                <h5 className='mt-2'>{item.food.item_name} ({item.food.item_quantity})</h5>
+                                <p>{item.food.item_description?.slice(0,70)}</p>
+                                <p><strong>Price:</strong> ₹{item.food.item_price}</p>
+                                <p><strong>Quantity: </strong>{item.quantity}</p>
                             </div>
                         </div>
                     </div>
                 ))}
             </div>
-            <div className='col-md-4'></div>
+            <div className='col-md-4'>
+                { orderAddress && (
+                    <div className='card shadow-sm py-4 border-0 bg-light'>
+                        <h5 className='fw-semibold mb-3'>
+                            <i className='fas fa-map-marker-alt me-2 text-danger'></i>Delivery Details</h5>
+                        <p className='ms-1'><strong>Date: </strong>{ new Date(orderAddress.order_time).toLocaleString()}</p>
+                        <p><strong>Address: </strong>{ orderAddress.address }</p>
+                        <p><strong>Status: </strong>{ orderAddress.order_final_status || 'Waiting for Resturant Confirmation'}</p>
+                        <p><strong>Payment Mode: </strong><span className='badge bg-info text-dark ms-2'>{ orderAddress.payment_mode }</span></p>
+                        <p><strong>Total: </strong> { total }</p>
+
+                    <a href="" target='_blank' className='btn btn-primary my-2 w-100'>
+                        <i className='fas fa-file-invoice me-2'></i> Invoice
+                    </a>
+                    <a href="" className='btn btn-danger my-2 w-100'>
+                        <i className='fas fa-times-circle me-2'></i>Cancle Order
+                    </a>
+                    </div>
+                )}
+            </div>
         </div>
     </div>
     </PublicLayout>
